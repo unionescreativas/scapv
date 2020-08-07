c92f146b642f77256299853d61e5e7f2a9d23c72
brayanjcuellar24 <brayanjcuellar@gmail.com>
2020-08-07 15:48:02 -0500

    Plantilla Integra a proyecto Nuxt

---
+++ frontend/components/Vito/Sidebars/SideBarStyle1.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="iq-sidebar">
+        <div class="iq-sidebar-logo d-flex justify-content-between">
+            <nuxt-link :to="homeURL">
+                <img :src="logo" class="img-fluid" alt="logo" />
+                <span>Vito</span>
+            </nuxt-link>
+            <div class="iq-menu-bt-sidebar" v-if="toggleButton">
+                <div class="iq-menu-bt align-self-center">
+                    <div class="wrapper-menu" @click="miniSidebar">
+                        <div class="main-circle">
+                            <i class="ri-arrow-left-s-line"></i>
+                        </div>
+                        <div class="hover-circle">
+                            <i class="ri-arrow-right-s-line"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="sidebar-scrollbar">
+            <nav class="iq-sidebar-menu" :class="horizontal ? 'd-xl-none' : ''">
+                <List :items="items" :open="true" :horizontal="horizontal" />
+            </nav>
+            <div class="p-3"></div>
+        </div>
+    </div>
+    <!-- TOP Nav Bar -->
+</template>
+
+<script>
+export default {
+    name: "SideBarStyle1",
+    props: {
+        homeURL: {
+            type: Object,
+            default: () => ({ name: "layout1.dashboard" })
+        },
+        items: { type: Array },
+        logo: {
+            type: String,
+            default: require("~/assets/images/logo.png")
+        },
+        horizontal: { type: Boolean },
+        toggleButton: { type: Boolean, default: true }
+    },
+    components: {
+        List: () => import("~/components/Vito/Menus/ListStyle1")
+    },
+    methods: {
+        miniSidebar() {
+            this.$emit("toggle");
+        }
+    },
+    data() {
+        return {};
+    }
+};
+</script>
---
+++ frontend/store/index.js
@@ -0,0 +1,12 @@
+export const state = () => ({
+    busy: false,
+    loggedIn: false,
+    strategy: "local",
+    user: false
+});
+
+export const mutations = {};
+
+export const actions = {};
+
+export const getters = {};
