485072142141bdff10c5ba889d35ceb980ee76ff
Daniel Alexander Paz Rodríguez <alexdaniel-1998@outlook.com>
2020-08-08 13:51:33 -0500

    Actualización frontend

--- frontend/store/Ecommerce/getters.js
+++
@@ -1,6 +0,0 @@
-export default {
-    cartState: (state) => state.cart,
-    cartCountState: (state) => state.cart.length,
-    wishlistState: (state) => state.wishlist,
-    selectedProductState: (state) => state.selectedProduct,
-};
