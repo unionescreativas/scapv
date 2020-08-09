d0f4a3f9322424e57ba6a5e38f036b43ac45251c
brayanjcuellar24 <brayanjcuellar@gmail.com>
2020-08-09 02:57:33 -0500

    Create Transferencias.php

---
+++ backend/Modules/Caracterizacion/Entities/Transferencias.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\Caracterizacion\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Webpatser\Uuid\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transferencias extends Model
+{
+    use SoftDeletes;
+    protected $table = 'transferencias';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    //protected $fillable = [];
+
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(
+            function ($model) {
+                $model->id = (string) Uuid::generate(4);
+            }
+        );
+    }
+
+    protected $casts = [
+        'id' => 'string'
+    ];
+}
