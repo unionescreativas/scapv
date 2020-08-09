f06ed4c0b333352e7a03bc50d39f9976faf2761b
brayanjcuellar24 <brayanjcuellar@gmail.com>
2020-08-09 00:43:45 -0500

    Creación Control de Actividad

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,5 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post("/login", "Auth\LoginController@login");
-Route::post("/logout", "Auth\LoginController@logout");
+// Route::post("/login", "Auth\LoginController@login");
+// Route::post("/logout", "Auth\LoginController@logout");
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
