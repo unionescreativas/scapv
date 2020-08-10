9996b1b8e3e21eb0aa3f39623441239cc21746eb
brayanjcuellar24 <brayanjcuellar@gmail.com>
2020-08-10 15:05:28 -0500

    Update CiudadanosController.php

--- backend/Modules/Caracterizacion/Http/Controllers/CiudadanosController.php
+++ backend/Modules/Caracterizacion/Http/Controllers/CiudadanosController.php
@@ -20,30 +20,37 @@ class CiudadanosController extends Controller
      */
     public function index(Request $request)
     {
-        ActivityLogger::activity("Consulto datos del modulo ciudadanos, Metodo Index");
+
         $length = $request->input('length');
         $sortBy = $request->input('column');
         $orderBy = $request->input('dir');
         $searchValue = $request->input('search');
-        $query = Ciudadano::eloquentQuery($sortBy, $orderBy, $searchValue);
+        ActivityLogger::activity("Consulto datos del modulo Ciudadanos,Parametros: Cantidad de registros: {$length}, Tipo de Ordenamiento:{$sortBy}, Campo para ordenar:{$orderBy}, Valor a Buscar {$searchValue}-> Metodo Index");
+        $query = Ciudadano::eloquentQuery($sortBy, $orderBy, $searchValue)->where('estado', '1');
         $data = $query->paginate($length);
         return new DataTableCollectionResource($data);
     }
 
     public function show($id)
     {
-        ActivityLogger::activity("Consulto datos del modulo ciudadanos para el registro: {$id}, Metodo show");
-        $ciudadano = Ciudadano::where('id', $id)->get();
-        if (is_null($ciudadano)) {
-            $ciudadano = Ciudadano::where('numero_documento', $id);
-            if (is_null($ciudadano)) {
-                return ['data' => "no existe", 'status' => '200'];
-            } else {
-                return ['data' => $ciudadano, 'status' => '201'];
-            }
+
+        $ciudadano = Ciudadano::where('id', $id)->where('estado', '1')->get();
+
+
+        if ($ciudadano->isEmpty()) {
+            $ciudadano = Ciudadano::where('numero_documento', $id)->get();
+            ActivityLogger::activity("Consulto datos del modulo Ciudadanos para el registro por cedula: {$id}, Valores consultados: {$ciudadano} -> Metodo show");
+            // if (!$ciudadano->isEmpty()) {
+            //     return ['data' => "no existe", 'status' => '200'];
+            // } else {
+
         } else {
-            return ['data' => $ciudadano, 'status' => '201'];
+            ActivityLogger::activity("Consulto datos del modulo Ciudadanos para el registro con id: {$id},  Valores consultados: {$ciudadano} -> Metodo show");
         }
+        if ($ciudadano->isEmpty()) {
+            return ['data' => 'no existe', 'status' => '201'];
+        }
+        return ['data' => $ciudadano, 'status' => '201'];
     }
 
 
@@ -89,12 +96,13 @@ class CiudadanosController extends Controller
         $ciudadanos->tipo_empleo = $request->tipo_empleo;
         $ciudadanos->observaciones = $request->observaciones;
         $ciudadanos->save();
-        ActivityLogger::activity("Guardando datos del modulo ciudadanos, Metodo Store. Datos :{$ciudadanos}");
+        ActivityLogger::activity("Guardando datos del modulo ciudadanos, Datos Guardaros:{$ciudadanos}, -> Metodo Store.");
         return ['data' => $ciudadanos, 'status' => '201'];
     }
     public function update(Request $request, $id)
     {
         //
+        $datosAnteriores = Ciudadano::find($id);
         $ciudadanos = Ciudadano::find($id);
         $ciudadanos->tipo_documento = $request->tipo_documento;
         $ciudadanos->numero_documento = $request->numero_documento;
@@ -134,41 +142,49 @@ class CiudadanosController extends Controller
         $ciudadanos->tipo_empleo = $request->tipo_empleo;
         $ciudadanos->observaciones = $request->observaciones;
         $ciudadanos->save();
-        ActivityLogger::activity("Actualizando datos del modulo ciudadanos, Metodo Update. Datos :{$ciudadanos}, para el registro id {$id}");
+        ActivityLogger::activity("Actualizando datos del modulo ciudadanos,  Datos Anteriores:{$datosAnteriores}  Datos Nuevos:{$ciudadanos}, para el registro id {$id} ->Metodo Update.");
         return ['data' => $ciudadanos, 'status' => '202'];
     }
     public function destroy($id)
     {
+        $ciudadanos = Ciudadano::find($id);
+        $datosElimnados = $ciudadanos;
         $ciudadanos = Ciudadano::destroy($id);
-        ActivityLogger::activity("Eliminado Registo Modulo ciudadanos, Metodo destroy {$id}");
+        ActivityLogger::activity("Eliminado Registo Modulo ciudadanos,Datos eliminados:{$datosElimnados},  para el registro {$id} -> Metodo destroy");
         return ['data' => $ciudadanos, 'status' => '203'];
+
+        //
+    }
+
+    public function activar($id)
+    {
+        $ciudadanos = Ciudadano::find($id);
+        $datosActivar = $ciudadanos;
+        ActivityLogger::activity("Activando Registo Modulo ciudadanos,Datos Activar: {$datosActivar}, para el registro {$id} -> Metodo Activar.");
+        $ciudadanos->restore();
+        return ['data' => $ciudadanos, 'status' => '202'];
 
         //
     }
 
     public function inactivar($id)
     {
-        ActivityLogger::activity("Inactivando Registo Modulo ciudadanos, Metodo inactivar {$id}");
+
         $ciudadanos = Ciudadano::find($id);
+        $datosActivar = $ciudadanos;
+        ActivityLogger::activity("Inactivando Registo Modulo ciudadanos,Datos Inactivar: {$datosActivar}, para el registro {$id} -> Metodo Inactivar.");
         $ciudadanos->estado = 0;
         $ciudadanos->save();
         return ['data' => $ciudadanos, 'status' => '202'];
 
         //
     }
-    public function activar($id)
-    {
-        ActivityLogger::activity("Activando Registo Modulo ciudadanos, Metodo activar {$id}");
-        $ciudadanos = Ciudadano::find($id);
-        $ciudadanos->restore();
-        return ['data' => $ciudadanos, 'status' => '202'];
-
-        //
-    }
     public function restore($id)
     {
-        ActivityLogger::activity("Recuperando Registro Borrado  Modulo ciudadanos, Metodo restore {$id}");
+
         $ciudadanos = Ciudadano::withTrashed()->find($id);
+        $datosRestaurar = $ciudadanos;
+        ActivityLogger::activity("Restaurando Registo Modulo ciudadanos,Datos a Restaurar: {$datosRestaurar}, para el registro {$id} -> Metodo Restaurar.");
         $ciudadanos->deleted_at = null;
         $ciudadanos->save();
         return ['data' => $ciudadanos, 'status' => '202'];
