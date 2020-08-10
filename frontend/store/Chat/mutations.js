022ec2f8bd51d0de4603ab036038102fbb68c46d
Daniel Alexander Paz Rodríguez <alexdaniel-1998@outlook.com>
2020-08-10 18:53:43 -0500

    Actualizar Proyecto

--- frontend/store/Chat/mutations.js
+++
@@ -1,8 +0,0 @@
-export default {
-    listCommit(state, data) {
-        state.chat = data;
-    },
-    videoCallCommit(state, data) {
-        state.videoCall = data;
-    }
-};
--- frontend/store/TicketBooking/getters.js
+++
@@ -1,5 +0,0 @@
-export default {
-    navTicket: state => state.navTicket,
-    numberOfTicket: state => state.numberOfTicket,
-    sidebarTicket: state => state.sidebarTicket
-};
