7d9b7a9c689dca13983f3c670b81afbeec601b55
Daniel Alexander Paz Rodríguez <alexdaniel-1998@outlook.com>
2020-08-11 11:43:58 -0500

    Consumiendo api para estadisticas

--- frontend/store/Estadisticas/actions.js
+++ frontend/store/Estadisticas/actions.js
@@ -4,7 +4,7 @@ export default {
   async consultarEstadisticas({ commit }, payload) {
     let estadisticas = null;
     let res = await this.$axios.get("/api/datosgenerales");
-    console.log(res);
-    // commit("ASIGNAR_CIUDADANO", estadisticas);
+    console.log(res)
+    commit("CONSULTAR_ESTADISTICAS", res.data.data);
   },
 };
