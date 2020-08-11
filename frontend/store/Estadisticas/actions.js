49f0b4a0f4e9eaedaf53769806939b24879c1e4e
Daniel Alexander Paz Rodríguez <alexdaniel-1998@outlook.com>
2020-08-11 12:49:07 -0500

    Consumiendo api para estadisticas

--- frontend/store/Estadisticas/actions.js
+++ frontend/store/Estadisticas/actions.js
@@ -2,9 +2,7 @@ import Swal from "sweetalert2";
 
 export default {
   async consultarEstadisticas({ commit }, payload) {
-    let estadisticas = null;
     let res = await this.$axios.get("/api/datosgenerales");
-    console.log(res)
     commit("CONSULTAR_ESTADISTICAS", res.data.data);
   },
 };
