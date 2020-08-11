2005479505c79ce4550e22d0036b52e44fa7a7d8
brayanjcuellar24 <brayanjcuellar@gmail.com>
2020-08-11 02:20:03 -0500

    Update api.php

--- backend/Modules/Estadisticas/Routes/api.php
+++ backend/Modules/Estadisticas/Routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/estadisticas', function (Request $request) {
     return $request->user();
 });
+
+Route::get('datosgenerales', 'DatosGeneralesController@nucleofamiliar')->name('datos.generales');
