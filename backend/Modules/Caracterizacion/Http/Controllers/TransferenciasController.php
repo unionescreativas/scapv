11fb424c84199d27efe87d5912a0c898d20f727c
brayanjcuellar24 <brayanjcuellar@gmail.com>
2020-08-10 22:50:35 -0500

    Update TransferenciasController.php

--- backend/Modules/Caracterizacion/Http/Controllers/TransferenciasController.php
+++ backend/Modules/Caracterizacion/Http/Controllers/TransferenciasController.php
@@ -10,6 +10,8 @@ use Illuminate\Contracts\Support\Renderable;
 use Modules\Caracterizacion\Entities\Transferencia;
 use jeremykenedy\LaravelLogger\App\Http\Traits\ActivityLogger;
 use JamesDordoy\LaravelVueDatatable\Http\Resources\DataTableCollectionResource;
+use Modules\Caracterizacion\Entities\Ciudadano;
+use Modules\Caracterizacion\Entities\Familia;
 
 class TransferenciasController extends Controller
 {
@@ -72,13 +74,74 @@ class TransferenciasController extends Controller
         $variableConsulta->familia_id = $request->familia_id;
         $variableConsulta->parentesco = $request->parentesco;
 
+        //   Datos antes de borrar------------------------------------->
+
+        $ciudadano = DB::table('ciudadanos')->where('id', $request->familia_id)->first();
+        $newCiudadano = new Familia;
+        $newCiudadano->ciudadano_id = $request->ciudadano_id;
+        $newCiudadano->parentesco = $request->parentesco;
+        $newCiudadano->tipo_documento = $ciudadano->tipo_documento;
+        $newCiudadano->numero_documento = $ciudadano->numero_documento;
+        $newCiudadano->pep = $ciudadano->pep;
+        $newCiudadano->nombres = $ciudadano->nombres;
+        $newCiudadano->apellidos = $ciudadano->apellidos;
+        $newCiudadano->fecha_expedicion = $ciudadano->fecha_expedicion;
+        $newCiudadano->fecha_vencimiento = $ciudadano->fecha_vencimiento;
+        $newCiudadano->fecha_nacimiento = $ciudadano->fecha_nacimiento;
+        $newCiudadano->edad = $ciudadano->edad;
+        $newCiudadano->genero = $ciudadano->genero;
+        $newCiudadano->estado_civil = $ciudadano->estado_civil;
+        $newCiudadano->telefono = $ciudadano->telefono;
+        $newCiudadano->celular = $ciudadano->celular;
+        $newCiudadano->correo_electronico = $ciudadano->correo_electronico;
+        $newCiudadano->departamento = $ciudadano->departamento;
+        $newCiudadano->ciudad = $ciudadano->ciudad;
+        $newCiudadano->barrrio = $ciudadano->barrrio;
+        $newCiudadano->comuna = $ciudadano->comuna;
+        $newCiudadano->dirrecion = $ciudadano->dirrecion;
+        $newCiudadano->actividad = $ciudadano->actividad;
+        $newCiudadano->ciudad_origen = $ciudadano->ciudad_origen;
+        $newCiudadano->pais_origen = $ciudadano->pais_origen;
+        $newCiudadano->fecha_llegada = $ciudadano->fecha_llegada;
+        $newCiudadano->intencion_ciudad = $ciudadano->intencion_ciudad;
+        $newCiudadano->respuesta_intencion = $ciudadano->respuesta_intencion;
+        $newCiudadano->fecha_llegada = $ciudadano->fecha_llegada;
+        $newCiudadano->discapacidad = $ciudadano->discapacidad;
+        $newCiudadano->salud = $ciudadano->salud;
+        $newCiudadano->estudia_actualmente = $ciudadano->estudia_actualmente;
+        $newCiudadano->nivel_escolaridad = $ciudadano->nivel_escolaridad;
+        $newCiudadano->tipo_profesion = $ciudadano->tipo_profesion;
+        $newCiudadano->comunidad_lgtbi = $ciudadano->comunidad_lgtbi;
+        $newCiudadano->comunidad_etnica = $ciudadano->comunidad_etnica;
+        $newCiudadano->trabajo = $ciudadano->trabajo;
+        $newCiudadano->tipo_empleo = $ciudadano->tipo_empleo;
+        $newCiudadano->observaciones = $ciudadano->observaciones;
+        $newCiudadano->save();
+
+        //   Datos antes de borrar------------------------------------->
+
         DB::table('familias')
             ->where('ciudadano_id', $request->familia_id)
             ->update(['ciudadano_id' => $request->ciudadano_id]);
 
         //Campos a guardar aquí--------------->
 
-        $variableConsulta->save();
+        //   Datos antes de borrar------------------------------------->
+
+        DB::table('ayudas')
+            ->where('ciudadano_id', $request->familia_id)
+            ->update(['ciudadano_id' => $request->ciudadano_id]);
+
+        //Campos a guardar aquí--------------->
+
+        //   Borrando Ciudadano
+        // DB::table('ciudadanos')->where('ciudadano_id', '=', $request->familia_id)->delete();
+        $eliminar = Ciudadano::withTrashed()->find($request->familia_id);
+        $eliminar->forceDelete();
+        //   Borrando Ciudadano------------------------------------->
+
+
+
         ActivityLogger::activity("Guardando datos del modulo {$this->modulo}, Datos Guardaros:{$variableConsulta}, -> Metodo Store.");
         return ['data' => $variableConsulta, 'status' => '202'];
     }
