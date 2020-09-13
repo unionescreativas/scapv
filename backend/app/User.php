9517300116064bf8df439085b09e14b8d023929a
brayanjcuellar24 <brayanjcuellar@gmail.com>
2020-09-12 20:54:26 -0500

    Tabla Usuarios

--- backend/app/User.php
+++ backend/app/User.php
@@ -2,14 +2,15 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Webpatser\Uuid\Uuid;
 
-class User extends Authenticatable
-{
-    use Notifiable;
-
+class User extends Authenticatable {
+    use SoftDeletes, Notifiable;
+    protected $keyType = 'string';
+    public $incrementing = false;
     /**
      * The attributes that are mass assignable.
      *
@@ -34,6 +35,15 @@ class User extends Authenticatable
      * @var array
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'id' => 'string', 'email_verified_at' => 'datetime',
     ];
+    public static function boot() {
+        parent::boot();
+        self::creating(
+            function ($model) {
+                $model->id = (string) Uuid::generate(4);
+            }
+        );
+    }
+
 }
