000ff88528bdb6a513530992044329087e205bcd
brayanjcuellar24 <brayanjcuellar@gmail.com>
2020-10-02 17:04:43 -0500

    ll

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->get('/ciudadanos', function (Request $request) {
+    return $request->user();
+});
+// Route::middleware('auth:sanctum')->get('/prueba', 'HomeController@prueba');
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -11,3 +11,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/prueba', 'HomeController@prueba');
